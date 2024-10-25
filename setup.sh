4fda25e27de94c4087bce9480a5ce2468edd106e
Cole Lashley <colel1410@gmail.com>
2024-10-25 18:16:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 echo "[+] Installing dependencies. Make sure you are on a debian based linux system." 
-sudo apt install gcc-avr gdb-avr binutils-avr avr-libc simavr libsimavr-dev
+sudo apt install gcc-avr gdb-avr binutils-avr avr-libc simavr libsimavr-dev gtkwave
 echo "[+] all packages installed"
